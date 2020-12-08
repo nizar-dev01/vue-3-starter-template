bd27563d82ff0e8914c8d89128053224857a93ca
Nizar K <nizar@iocod.com>
2020-12-08 23:33:14 +0530

    basic template

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import indexPlugin from '@/plugins'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
+app.use(indexPlugin)
---
+++ src/mixins/src/methods.js
@@ -0,0 +1,5 @@
+export default {
+    exampleMixinMethod(){
+        console.log('hello mixin :)')
+    }
+}
